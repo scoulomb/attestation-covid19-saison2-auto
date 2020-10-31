6409a3d224bea12a51b924331b1e32cff6d74d52
Sylvain COULOMBEL <sylvaincoulombel@gmail.com>
2020-10-31 14:50:22 +0100

    Automation via HTTP query parameters

--- src/js/main.js
+++ src/js/main.js
@@ -2,85 +2,17 @@ import { generatePdf } from './pdf-util'
 import pdfBase from '../certificate.pdf'
 
 console.log("Main")
+var valid_link = "http://localhost:1234/?address=15 rue d\'Antibes&birthday=20/03/1882&city=Antibes&firstname=Rene&minutesoffset=60&lastname=Coty&placeofbirth=Le Havre&zipcode=06600&reason=sport_animaux" 
 
-function make_date_str(today) {
-
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  var today_str = dd + '/' + mm + '/' + yyyy;
-  return today_str
-}
-
-function make_time_str(today) { 
-  return String(today.getHours()).padStart(2, '0') + ":" + String(today.getMinutes()).padStart(2, '0')
-}
-
-
-const today = new Date()
-var today_str = make_date_str(today)
-var time_str_1 = make_time_str(today)
-today.setHours(today.getHours() + 1) 
-var time_str_2 =  make_time_str(today)
-
-/***** Fill your data here *****/
-
-// It will generate a first certicate where heure de sortie is  generation time and reason 1
-// Followed by a second certicate where heure de sortie is generation time and reason 2
-var profile1 = 
-	{    
-        "address": "8 rue des bains",
-	"birthday": "24/11/1967",
-	"city": "Antibes",
-	"datesortie": today_str,
-	"firstname": "Sylvain",
-	"heuresortie": time_str_1,
-	"lastname": "Yolo",
-	"placeofbirth": "Paris",
-	"zipcode": "06600"
-	}  
-	
-
-// const reason1 =  "travail"
-// const reason1 =  "achats"
-// const reason1 =  "sante"
-// const reason1 =  "famille"
-// const reason1 =  "handicap"
-const reason1 =  "sport_animaux"
-// const reason1 =  "convocation"
-// const reason1 =  "missions"
-// const reason1 =  "enfants"
-
-
-// const reason2 =  "travail"
-const reason2 =  "achats"
-// const reason2 =  "sante"
-// const reason2 =  "famille"
-// const reason2 =  "handicap"
-// const reason2 =  "sport_animaux"
-// const reason2 =  "convocation"
-// const reason2 =  "missions"
-// const reason2 =  "enfants"
-
-/***** Fill your data here *****/
-
-var profile2 = profile1
-var profile2 = Object.assign({}, profile1); // deep copy
-profile2["heuresortie"] = time_str_2
- 
-
-generatePdf(profile1, reason1, pdfBase)
 // https://stackoverflow.com/questions/18239430/cannot-set-property-innerhtml-of-null
 // https://stackoverflow.com/questions/37951999/error-message-innerhtml-is-not-a-function
-var node = document.getElementById('status');
-node.innerHTML = '<p>Certificate 1 generated</p>';
+var status = document.getElementById('status');
+status.innerHTML = '<p>Started</p>';
+var errors = document.getElementById('errors');
+var help = document.getElementById('help');
+help.innerHTML = "<p> <a href=\"" + valid_link + "\"> Example of valid usage </a></p>"
 
-generatePdf(profile2, reason2, pdfBase)
-node.innerHTML = '<p>Certificate 1 and 2 generated. Browser can be close<br>Thanks for using</p>';
-
-// set downloadBlob in generatePdf otheriwse issue with async, it download before generation endee
-
-// 
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
 function getParameterByName(name, url = window.location.href) {
     console.log(url)
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -90,38 +22,102 @@ function getParameterByName(name, url = window.location.href) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }   
-// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-var foo = getParameterByName('foo'); // "lorem"
-node.innerHTML = foo;
 
 
-// http://localhost:1234/?address=8 rue des bains&birthday=24/11/1989&city=Antibes&datesortie=31/10/2020&firstname=Sylvain&heuresortie=02:05&lastname=Coulombel&placeofbirth=vsg&zipcode=06600
+function make_date_str(date) {
 
-var address = getParameterByName('address');
-var birthday = getParameterByName('birthday');
-var city = getParameterByName('city');
-var datesortie = getParameterByName('datesortie');
-var firstname = getParameterByName('firstname');
-var heuresortie = getParameterByName('heuresortie');
-var lastname = getParameterByName('lastname');
-var placeofbirth = getParameterByName('placeofbirth');
-var zipcode = getParameterByName('zipcode');
+  var dd = String(date.getDate()).padStart(2, '0');
+  var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = date.getFullYear();
+  var date_str = dd + '/' + mm + '/' + yyyy;
+  return date_str
+}
+
+function make_time_str(date) { 
+  return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0')
+}
+
+function process() {
+
+  var inputElementKeys = ['address', 'birthday', 'city', 'firstname', 'minutesoffset', 'lastname', 'placeofbirth', 'zipcode', 'reason'] 
+  var  inputMap = new Map();
+
+  // Fill input map and check all elements are present
+  inputElementKeys.forEach(key => inputMap.set(key, getParameterByName(key)));
+
+  var nullKeys = [] 
+  for (const  [key, value] of inputMap.entries()) {
+    console.log(value);
+  
+    if(value===null){
+       nullKeys.push(key)
+    }
+  }
+  
+  if (nullKeys.length > 0)
+  {
+       help.innerHTML = ""
+       status.innerHTML = '<p style="color:#FF0000"; ><b>User errors</b></p>';
+       errors.innerHTML =  "<p> Mandatory keys: [" +   nullKeys.join(", ")   + "] are missing. </p> <br>" +
+       "<p> An exampe of valid usage is </p>" +
+       "<p> <a href=\"" +valid_link + "\">" + valid_link +  "</a></p>"
+       return;    
+  }
 
 
-var profile = 
-	{    
-        "address": address,
-	"birthday": birthday,
-	"city": city,
-	"datesortie": datesortie,
-	"firstname": firstname,
-	"heuresortie": heuresortie,
-	"lastname": lastname,
-	"placeofbirth": placeofbirth,
-	"zipcode": zipcode
-	}  
-	
-node.innerHTML = JSON.stringify(profile)
+  // Check reason is valid otherwise PDF generation is failing
+  var accepted_reasons = [
+    "travail",
+    "achats",
+    "sante",
+    "famille",
+    "handicap",
+    "sport_animaux",
+    "convocation",
+    "missions",
+    "enfants",
+  ]
 
+  console.log(inputMap.get("reason"))
+  var accepted = accepted_reasons.includes(inputMap.get("reason"));
+  if(! accepted) {    
+    status.innerHTML = "<p> Reason not accepted </p>"
+    errors.innerHTML =  "<p> Valid reasons are: " + accepted_reasons.join(", ") + "</p>"
+    return;
+  }
 
-generatePdf(profile, reason1, pdfBase)
+  // Process
+  
+  /// Compute date offset 
+  var now = new Date ()
+  console.log("now: " + now)
+  var now_with_offset = new Date (now);
+  now_with_offset.setMinutes ( now.getMinutes() + parseInt(inputMap.get("minutesoffset")));
+  console.log("now with offset:" + now_with_offset) 
+
+  var date_str = make_date_str(now_with_offset)
+  var time_str = make_time_str(now_with_offset)
+  
+  /// Compute profile
+  var profile = 
+    {    
+    "address": inputMap.get("address"),
+    "birthday": inputMap.get("birthday"),
+    "city": inputMap.get("city"),
+    "datesortie": date_str,
+    "firstname": inputMap.get("firstname"),
+    "heuresortie": time_str,
+    "lastname": inputMap.get("lastname"),
+    "placeofbirth": inputMap.get("placeofbirth"),
+    "zipcode": inputMap.get("zipcode")
+    }
+  status.innerHTML = JSON.stringify(profile)
+  
+  /// Generate pdf
+  generatePdf(profile, inputMap.get("reason"), pdfBase)
+  // set downloadBlob in generatePdf otheriwse issue with async, it download before generation ended
+  status.innerHTML = '<p>Certificate generation completed with profile:</p>' + '<p>' + JSON.stringify(profile)  + '<p>';
+
+}
+
+process()
